78c1b59ac38efaaf6e4c8f07d6bb23ef3603ba8b
sbasaranoglu <suleyman.basaranoglu@hepsiburada.com>
2022-01-04 17:23:53 +0300

    Clean some code

--- app/src/main/java/com/example/getdataapi/MainActivity.kt
+++ app/src/main/java/com/example/getdataapi/MainActivity.kt
@@ -1,63 +1,38 @@
 package com.example.getdataapi
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.example.getdataapi.databinding.ActivityMainBinding
 
+const val BASE_URL = "https://www.omdbapi.com/?apikey=dad2aa7f&t="
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var  getDataText : TextView
-    lateinit var getDataButton : Button
-    lateinit var  getDataSearchButton : Button
-
+    private lateinit var _binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-
-        getDataSearchButton = findViewById(R.id.getDataSearchButton)
-        getDataButton = findViewById(R.id.getDataButton)
-        getDataText = findViewById(R.id.getDataText)
-
-        getDataButton.setOnClickListener{
-            val queue = Volley.newRequestQueue(this)
-            val url = "https://www.omdbapi.com/?apikey=dad2aa7f&t=lord"
-            val stringRequest  = StringRequest(
-                Request.Method.GET, url,
-                Response.Listener <String> { response ->
-                    Toast.makeText(applicationContext, response. toString(), Toast.LENGTH_SHORT).show()
-                },
-                Response.ErrorListener { err ->
-                    Toast.makeText(applicationContext, err.toString(), Toast.LENGTH_SHORT).show()
-                })
-            queue.add(stringRequest)
-        }
-
-        getDataSearchButton.setOnClickListener{
-
-            val queue = Volley.newRequestQueue(this)
-            val url = "https://www.omdbapi.com/?apikey=dad2aa7f&t=" + getDataText.getText().toString()
-            val stringRequest  = StringRequest(
-                Request.Method.GET, url,
-                Response.Listener <String> { response ->
-                    Toast.makeText(applicationContext, response.toString(), Toast.LENGTH_SHORT).show()
-                },
-                Response.ErrorListener { err ->
-                    Toast.makeText(applicationContext, err.toString(), Toast.LENGTH_SHORT).show()
-                })
-            queue.add(stringRequest)
-        }
-
-        }
-
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+        getDataFromAPI()
     }
 
+    fun getDataFromAPI() {
+        with(_binding){
+            btnSearchMovie.setOnClickListener {
+                val queue = Volley.newRequestQueue(this@MainActivity)
+                val url = BASE_URL + etMovieName.text.toString()
+                val stringRequest = StringRequest(Request.Method.GET, url, { response ->
+                    Toast.makeText(applicationContext, response.toString(), Toast.LENGTH_SHORT).show()
+                }, { err ->
+                    Toast.makeText(applicationContext, err.toString(), Toast.LENGTH_SHORT).show()
+                })
+                queue.add(stringRequest)
+            }
+        }
+    }
+}
